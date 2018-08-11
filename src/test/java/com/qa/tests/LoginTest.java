d52d93b8fe1c7b42ff4c19422ba6b9e2badfe2e9
bharathchillamcharla <chillamcharla.bharath@gmail.com>
2018-08-11 22:13:32 +0530

    first git commit for selenium code

---
+++ src/test/java/com/qa/tests/LoginTest.java
@@ -0,0 +1,14 @@
+package com.qa.tests;
+
+import org.testng.annotations.Test;
+
+public class LoginTest {
+	
+	@Test
+	public void test()
+	{
+		System.out.println("this is the test method");
+		
+	}
+
+}
